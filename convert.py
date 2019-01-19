5979b998beeef397587a8019db31354cfe77176f
Klercken <kenny.deklerck@gmail.com>
2019-01-19 22:14:06 +0100

    Update convert.py
    Bug fix group class:
    - all in absolute coördinates
    
    Solution for brackets, only minimum required number of brackets

--- convert.py
+++ convert.py
@@ -1,18 +1,18 @@
 # Kenny De Klerck
-# 2018.11.17
+# 2019.01.19
 
 # classes
 class Group:
     """__init__() class constructor"""
-    def __init__(self, id, x, y, l, w):
+    def __init__(self, id, x1, y1, x2, y2, d):
         self.id = id
-        self.x1 = x
-        self.y1 = y
-        self.l = l
-        self.w = w
-        self.x2 = x + l
-        self.y2 = y + w
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
         self.c = False
+        # direction: <empty>: none, V = /-division, H = ,-division
+        self.d = d
 
 
 # functions
@@ -63,11 +63,24 @@ def checkValid (first=Group, second=Group):
 # main routine
 if __name__ == "__main__":
     # rectangle input list
-    coordinates = [["a",0,0,400,200]
-        ,["d",0,200,800,200]
-        ,["c",0,400,1000,600]
-        ,["b",400,0,400,200]
-        ,["e",800,0,300,200]]
+    # coordinates = [["a",0,0,400,200]
+    #     ,["d",0,200,800,200]
+    #     ,["c",0,400,1000,600]
+    #     ,["b",400,0,400,200]
+    #     ,["e",800,0,300,200]]
+
+    coordinates = [["a", 0, 50, 40, 140]
+        ,["b", 50, 160, 110, 30]
+        ,["c", 50, 120, 40, 30]
+        ,["d", 100, 140, 30, 10]
+        ,["e", 100, 120, 30, 10]
+        ,["f", 140, 120, 20, 30]
+        ,["g", 50, 80, 110, 30]
+        ,["h", 50, 50, 70, 20]
+        ,["i", 130, 50, 30, 20]
+        ,["j", 0, 0, 160, 40]
+        ,["k", 170, 0, 50, 190]
+        ]
 
     # lists
     groupInList = []
@@ -75,11 +88,11 @@ if __name__ == "__main__":
 
     # create objects from input data
     for item in coordinates:
-        groupInList.append(Group(item[0], item[1], item[2], item[3], item[4]))
+        groupInList.append(Group(item[0], item[1], item[2], item[1] + item[3], item[2] + item[4], ""))
     
     groupOutList = groupInList.copy()
 
-    # while routine to iteratie figue definition
+    # while routine to iteratie figure definition
     while len(groupOutList) > 1:
         # reset group out
         groupOutList.clear()
@@ -91,7 +104,7 @@ if __name__ == "__main__":
                 # run through groups for second item in combination
                 for secondGroup in groupInList:
                     # if same as first group, or in use, skip
-                    if not (firstGroup.id == secondGroup.id or secondGroup.c):
+                    if not (firstGroup.id == secondGroup.id or firstGroup.c or secondGroup.c):
                         # check valid combination
                         valid = checkValid (firstGroup, secondGroup)
 
@@ -102,24 +115,63 @@ if __name__ == "__main__":
                             secondGroup.c = True
 
                             # create combination id
+                            # 1 left of 2
                             if firstGroup.x2 <= secondGroup.x1:
-                                combId = "(" + firstGroup.id + "," + secondGroup.id + ")"
+                                d = "H"
+                                if (firstGroup.d == "V" and secondGroup.d == "V"):
+                                    combId = "(" + firstGroup.id + "),(" + secondGroup.id + ")"
+                                elif (firstGroup.d == "V"):
+                                    combId = "(" + firstGroup.id + ")," + secondGroup.id
+                                elif (secondGroup.d == "V"):
+                                    combId = firstGroup.id + ",(" + secondGroup.id + ")"
+                                else:
+                                    combId = firstGroup.id + "," + secondGroup.id
+
+                            # 1 right of 2
                             elif firstGroup.x1 >= secondGroup.x2:
-                                combId = "(" + secondGroup.id + "," + firstGroup.id + ")"
-                            elif firstGroup.y2 <= secondGroup.y1:
-                                combId = "(" + firstGroup.id + "/" + secondGroup.id + ")"
+                                d = "H"
+                                if (secondGroup.d == "V" and firstGroup.d == "V"):
+                                    combId = "(" + secondGroup.id + "),(" + firstGroup.id + ")"
+                                elif (secondGroup.d == "V"):
+                                    combId = "(" + secondGroup.id + ")," + firstGroup.id
+                                elif (firstGroup.d == "V"):
+                                    combId = secondGroup.id + ",(" + firstGroup.id + ")"
+                                else:
+                                    combId = secondGroup.id + "," + firstGroup.id
+
+                            # 1 below 2
+                            elif firstGroup.y1 >= secondGroup.y2:
+                                d = "V"
+                                if (firstGroup.d == "H" and secondGroup.d == "H"):
+                                    combId = "(" + firstGroup.id + ")/(" + secondGroup.id + ")"
+                                elif (firstGroup.d == "H"):
+                                    combId = "(" + firstGroup.id + ")/" + secondGroup.id
+                                elif (secondGroup.d == "H"):
+                                    combId = firstGroup.id + "/(" + secondGroup.id + ")"
+                                else:
+                                    combId = firstGroup.id + "/" + secondGroup.id
+                                    
+                            # 1 above 2
                             else:
-                                combId = "(" + secondGroup.id + "/" + firstGroup.id + ")"
+                                d = "V"
+                                if (secondGroup.d == "H" and firstGroup.d == "H"):
+                                    combId = "(" + secondGroup.id + ")/(" + firstGroup.id + ")"
+                                elif (secondGroup.d == "H"):
+                                    combId = "(" + secondGroup.id + ")/" + firstGroup.id
+                                elif (firstGroup.d == "H"):
+                                    combId = secondGroup.id + "/(" + firstGroup.id + ")"
+                                else:
+                                    combId = secondGroup.id + "/" + firstGroup.id
 
                             # append combination to groupOutList
                             bounding = findBounding (firstGroup, secondGroup)
-                            groupOutList.append(Group(combId, bounding[0], bounding[1], bounding[2], bounding[3]))
+                            groupOutList.append(Group(combId, bounding[0], bounding[1], bounding[2], bounding[3], d))
 
         
         # add remaining not grouped items in new group
         for item in groupInList:
             if not (item.c):
-                groupOutList.append(Group(item.id, item.x1, item.y1, item.x2, item.y2))
+                groupOutList.append(Group(item.id, item.x1, item.y1, item.x2, item.y2, item.d))
 
         # set new group for loop
         groupInList = groupOutList.copy()
